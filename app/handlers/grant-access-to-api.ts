0dac5d771b368b4cbbdfec126b6593df4764f641
mpgxc <mpgx5.c@gmail.com>
2025-01-18 13:41:25 -0300

    Add API handler for granting access and implement picture search functionality

---
+++ app/handlers/grant-access-to-api.ts
@@ -0,0 +1,12 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  const token =
+    event.headers?.Authorization || event.headers?.authorization || "";
+
+  console.info(token);
+
+  return {
+    isAuthorized: true,
+  };
+};
