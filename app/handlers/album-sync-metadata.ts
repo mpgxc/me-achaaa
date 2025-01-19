e316c1aeb98db7f379ff88b5de4e3e84d00e299a
mpgxc <mpgx5.c@gmail.com>
2025-01-19 00:19:46 -0300

    Add SQS integration for album metadata synchronization and update serverless configuration

---
+++ app/handlers/album-sync-metadata.ts
@@ -0,0 +1,15 @@
+export type SyncMetadataEvent = {
+  bucket: string;
+  /**
+   * The id of the image in the rekognition collection
+   */
+  imageId: string;
+  /**
+   * The key of the image in the bucket
+   */
+  key: string;
+};
+
+export const handler = async (records: SyncMetadataEvent[]): Promise<void> => {
+  console.log("SyncMetadataEvent", JSON.stringify(records, null, 2));
+};
