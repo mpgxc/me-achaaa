fc008784a332b9d2a4be5923211e20baab05d779
mpgxc <mpgx5.c@gmail.com>
2025-01-20 19:07:28 -0300

    update

--- app/handlers/picture-index-processing.ts
+++ app/handlers/picture-index-processing.ts
@@ -1,49 +1,58 @@
-import { BatchWriteItemCommand } from "@aws-sdk/client-dynamodb";
 import {
-  Face,
-  FaceRecord,
-  IndexFacesCommand,
-} from "@aws-sdk/client-rekognition";
-import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+  TransactWriteItem,
+  TransactWriteItemsCommand,
+} from "@aws-sdk/client-dynamodb";
+import { Face, IndexFacesCommand } from "@aws-sdk/client-rekognition";
 import { marshall } from "@aws-sdk/util-dynamodb";
 import { S3Event, S3EventRecord, SQSBatchResponse, SQSEvent } from "aws-lambda";
-import sharp from "sharp";
-import {
-  DynamoSingleton,
-  RekognitionSingleton,
-  S3Singleton,
-} from "../providers.js";
+import { DynamoSingleton, RekognitionSingleton } from "../providers.js";
+
+const TRANSACTIONS_LIMIT_PER_BATCH = 50;
 
 const rekognition = RekognitionSingleton.getInstance();
-const s3Client = S3Singleton.getInstance();
 const dynamodb = DynamoSingleton.getInstance();
 
 const extractExternalImageId = (key: string) => {
-  const [, CollectionId, ExternalImageId] = key.split("/");
+  const [CollectionId, ExternalImageId] = key
+    .replace("uploads/incoming/", "")
+    .replace(".jpg", "")
+    .split("/");
+
   return {
     CollectionId,
     ExternalImageId,
   };
 };
 
-const rekognitionEventHandler = async (
-  s3: S3EventRecord["s3"]
-): Promise<FaceRecord[]> => {
-  try {
-    console.info(
-      `Processing S3 object <${s3.object.key}> from bucket <${s3.bucket.name}>`
-    );
+const splitBatches = (items: TransactWriteItem[], batchSize = 100) => {
+  return items.reduce((batches, item, index) => {
+    const batchIndex = Math.floor(index / batchSize);
 
+    if (!batches[batchIndex]) {
+      batches[batchIndex] = [];
+    }
+
+    batches[batchIndex].push(item);
+
+    return batches;
+  }, [] as TransactWriteItem[][]);
+};
+
+const rekognitionEventHandler = async ({
+  bucket,
+  object,
+}: Pick<S3EventRecord["s3"], "bucket" | "object">): Promise<Face[]> => {
+  try {
     const { ExternalImageId, CollectionId } = extractExternalImageId(
-      s3.object.key
+      object.key
     );
 
     const command = new IndexFacesCommand({
       CollectionId,
       Image: {
         S3Object: {
-          Bucket: s3.bucket.name,
-          Name: s3.object.key,
+          Bucket: bucket.name,
+          Name: object.key,
         },
       },
       MaxFaces: 3,
@@ -52,184 +61,26 @@ const rekognitionEventHandler = async (
       DetectionAttributes: ["ALL"],
     });
 
-    const output = await rekognition.send(command);
+    const { FaceRecords } = await rekognition.send(command);
 
-    if (!output.FaceRecords || output.FaceRecords.length === 0) {
-      console.warn(`No faces were indexed for image <${s3.object.key}>.`);
+    if (!FaceRecords || !FaceRecords.length) {
+      console.warn(
+        `Rekognition: No faces were indexed for image ${object.key}`
+      );
+
+      return [];
     }
 
     console.info(
-      `S3 object <${s3.object.key}> indexed successfully in collection <${CollectionId}>`
+      `Rekognition: ${FaceRecords?.length} faces indexed for image ${object.key}`
     );
 
-    return output?.FaceRecords || [];
+    return FaceRecords.flatMap(({ Face }) => Face || []);
   } catch (error) {
     console.error(
-      `Error processing S3 event: ${JSON.stringify(error, null, 2)}`
-    );
-
-    return [];
-  }
-};
-
-const getImageFromBucket = async (
-  s3: S3EventRecord["s3"]
-): Promise<Uint8Array> => {
-  const { Body } = await s3Client.send(
-    new GetObjectCommand({
-      Bucket: s3.bucket.name,
-      Key: s3.object.key,
-    })
-  );
-
-  return Body!.transformToByteArray();
-};
-
-const generateThumbnail = async (image: Uint8Array): Promise<Uint8Array> => {
-  const thumbnail = await sharp(image)
-    .resize({ width: 256, height: 256, fit: "inside" })
-    .toBuffer();
-
-  return thumbnail;
-};
-
-const generateThumbnailWithWaterMark = async (
-  image: Uint8Array
-): Promise<Uint8Array> => {
-  const watermarkPath = __dirname + "/watermark.png";
-  const thumbnail = await sharp(image)
-    .resize({ width: 256, height: 256, fit: "inside" })
-    .composite([
-      {
-        input: watermarkPath,
-        gravity: "southeast",
-      },
-    ])
-    .toBuffer();
-
-  return thumbnail;
-};
-
-const cropFacesEventHandler = async (
-  s3: S3EventRecord["s3"],
-  faces: FaceRecord[]
-): Promise<string[]> => {
-  try {
-    const croppeds: string[] = [];
-
-    const image = await getImageFromBucket(s3);
-
-    if (!image) {
-      console.error("Failed to retrieve image from S3.");
-
-      return [];
-    }
-
-    /**
-     * @todo Remover daqui e adicionar em um novo handler
-     */
-    {
-      const { CollectionId, ExternalImageId } = extractExternalImageId(
-        s3.object.key
-      );
-      const thumbnailPath = `uploads/${CollectionId}/thumbnails/${ExternalImageId}.jpg`;
-      const watermarkThumbnailPath = `uploads/${CollectionId}/thumbnails/${ExternalImageId}-watermark.jpg`;
-
-      await generateThumbnail(image).then(async (thumbnail) => {
-        const command = new PutObjectCommand({
-          Bucket: s3.bucket.name,
-          Key: thumbnailPath,
-          Body: thumbnail,
-          ContentType: "image/jpeg",
-        });
-
-        try {
-          await s3Client.send(command);
-          console.info(`Thumbnail uploaded to ${thumbnailPath}`);
-        } catch (error) {
-          console.error(
-            `Error uploading thumbnail to ${thumbnailPath}: ${error}`
-          );
-        }
-      });
-
-      await generateThumbnailWithWaterMark(image).then(async (thumbnail) => {
-        const command = new PutObjectCommand({
-          Bucket: s3.bucket.name,
-          Key: watermarkThumbnailPath,
-          Body: thumbnail,
-          ContentType: "image/jpeg",
-        });
-
-        try {
-          await s3Client.send(command);
-          console.info(
-            `Watermark thumbnail uploaded to ${watermarkThumbnailPath}`
-          );
-        } catch (error) {
-          console.error(
-            `Error uploading watermark thumbnail to ${watermarkThumbnailPath}: ${error}`
-          );
-        }
-      });
-    }
-
-    const metadata = await sharp(image).metadata();
-
-    if (!metadata.width || !metadata.height) {
-      console.warn("Image dimensions not available.");
-
-      return [];
-    }
-
-    const { CollectionId } = extractExternalImageId(s3.object.key);
-
-    for (const { Face } of faces) {
-      if (!Face || !Face.BoundingBox || !Face.FaceId) continue;
-
-      const { FaceId, BoundingBox } = Face;
-      const { Left, Top, Width, Height } = BoundingBox;
-
-      const left = Math.round(Left! * metadata.width);
-      const top = Math.round(Top! * metadata.height);
-      const width = Math.round(Width! * metadata.width);
-      const height = Math.round(Height! * metadata.height);
-
-      try {
-        const croppedImage = await sharp(image)
-          .extract({
-            left,
-            top,
-            width,
-            height,
-          })
-          .toBuffer();
-
-        {
-          const command = new PutObjectCommand({
-            Bucket: s3.bucket.name,
-            Key: `uploads/${CollectionId}/faces/${FaceId}.jpg`,
-            Body: croppedImage,
-            ContentType: "image/jpeg",
-          });
-
-          await s3Client.send(command);
-        }
-
-        croppeds.push(FaceId);
-      } catch (error) {
-        console.error(
-          `Error cropping face ${FaceId}: ${
-            error instanceof Error ? error.message : JSON.stringify(error)
-          }`
-        );
-      }
-    }
-
-    return croppeds;
-  } catch (error) {
-    console.error(
-      `Error processing S3 event: ${JSON.stringify(error, null, 2)}`
+      `Rekognition: Error processing S3 event: ${
+        error instanceof Error ? error.message : JSON.stringify(error)
+      }`
     );
 
     return [];
@@ -239,37 +90,39 @@ const cropFacesEventHandler = async (
 export const handler = async ({
   Records,
 }: SQSEvent): Promise<SQSBatchResponse> => {
-  const processedImages: Array<{
-    s3Key: string;
+  const batchItemFailures: Array<{ itemIdentifier: string }> = [];
+
+  const images: Array<{
+    key: string;
     faces: Face[];
   }> = [];
 
-  const batchItemFailures: Array<{ itemIdentifier: string }> = [];
-
   for (const { body, messageId } of Records) {
-    const { Records } = JSON.parse(body) as S3Event;
-    const [{ s3 }] = Records;
-
     try {
-      const faces = await rekognitionEventHandler(s3);
+      const { Records } = JSON.parse(body) as S3Event;
+
+      const [
+        {
+          s3: { bucket, object },
+        },
+      ] = Records;
+
+      const faces = await rekognitionEventHandler({
+        bucket,
+        object,
+      });
 
       if (!faces.length) {
-        console.warn(`No faces found for S3 object <${s3.object.key}>.`);
-
-        continue;
+        console.warn(`Handler: No faces were indexed for image: ${object.key}`);
       }
 
-      const croppeds = (await cropFacesEventHandler(s3, faces)) || [];
-
-      processedImages.push({
-        s3Key: decodeURIComponent(s3.object.key),
-        faces: faces
-          .filter(({ Face }) => croppeds.includes(Face?.FaceId!))
-          .map(({ Face }) => Face!),
+      images.push({
+        key: object.key,
+        faces,
       });
     } catch (error) {
       console.error(
-        `Error processing S3 event: ${
+        `Handler: Error processing S3 event: ${
           error instanceof Error ? error.message : JSON.stringify(error)
         }`
       );
@@ -277,14 +130,12 @@ export const handler = async ({
       batchItemFailures.push({
         itemIdentifier: messageId,
       });
-
-      continue;
     }
   }
 
-  await registerImageMetadata(processedImages).catch((error) => {
+  await registerImageMetadata(images).catch((error) => {
     console.error(
-      `Error registering image metadata: ${
+      `RegisterImageMetadata: Error registering image metadata: ${
         error instanceof Error ? error.message : JSON.stringify(error)
       }`
     );
@@ -294,77 +145,92 @@ export const handler = async ({
 };
 
 const registerImageMetadata = async (
-  images: Array<{
-    s3Key: string;
-    faces: Face[];
-  }>
+  images: Array<{ key: string; faces: Face[] }>
 ) => {
-  const allfaces = [] as {
+  const facesToStore = [] as Array<{
     CollectionId: string;
     faces: Face[];
-  }[];
+  }>;
 
-  const command = new BatchWriteItemCommand({
-    RequestItems: {
-      [dynamodb.tableName]: images.map(({ s3Key, faces }) => {
-        const { CollectionId, ExternalImageId } = extractExternalImageId(s3Key);
+  const TransactItems = images.map(({ key, faces }) => {
+    const { CollectionId, ExternalImageId } = extractExternalImageId(key);
 
-        allfaces.push({ CollectionId, faces });
+    facesToStore.push({
+      CollectionId,
+      faces,
+    });
 
-        return {
-          PutRequest: {
-            Item: marshall({
-              PK: `ALBUM#${CollectionId}`,
-              SK: `IMAGE#${ExternalImageId}`,
-              ExternalImageId,
-              Content: {
-                thumbnail: `${CollectionId}/thumbnails/${ExternalImageId}.jpg`,
-                original: `${CollectionId}/originals/${ExternalImageId}.jpg`,
-                faces: faces.map(
-                  ({ FaceId }) => `${CollectionId}/faces/${FaceId!}.jpg`
-                ),
-              },
-              CreatedAt: new Date().toISOString(),
-            }),
-          },
-        };
-      }),
-    },
-  });
-
-  await dynamodb.send(command);
-
-  await registerFacesImageMetadata(allfaces);
-};
-
-const registerFacesImageMetadata = async (
-  Faces: {
-    CollectionId: string;
-    faces: Face[];
-  }[]
-) => {
-  const Items = Faces.flatMap(({ CollectionId, faces }) =>
-    faces.map(({ FaceId, ImageId, ExternalImageId, Confidence }) => ({
-      PutRequest: {
+    return {
+      Put: {
+        TableName: dynamodb.tableName,
         Item: marshall({
           PK: `ALBUM#${CollectionId}`,
-          SK: `FACE#${FaceId}`,
-          CollectionId,
+          SK: `IMAGE#${ExternalImageId}`,
           ExternalImageId,
-          FaceId,
-          ImageId,
-          Confidence,
+          Content: {
+            thumbnail: `${CollectionId}/thumbnails/${ExternalImageId}.jpg`,
+            original: `${CollectionId}/originals/${ExternalImageId}.jpg`,
+            faces: faces.map(
+              ({ FaceId }) => `${CollectionId}/faces/${FaceId!}.jpg`
+            ),
+          },
           CreatedAt: new Date().toISOString(),
         }),
       },
-    }))
-  );
+    };
+  }) as TransactWriteItem[];
 
-  const command = new BatchWriteItemCommand({
-    RequestItems: {
-      [dynamodb.tableName]: Items,
-    },
-  });
+  const batches = splitBatches(TransactItems, TRANSACTIONS_LIMIT_PER_BATCH);
 
-  await dynamodb.send(command);
+  for (const batch of batches) {
+    const command = new TransactWriteItemsCommand({
+      TransactItems: batch,
+    });
+
+    await dynamodb.send(command);
+  }
+
+  await registerFacesImageMetadata(facesToStore);
+};
+
+const registerFacesImageMetadata = async (
+  images: Array<{
+    CollectionId: string;
+    faces: Face[];
+  }>
+) => {
+  const TransactItems = [] as TransactWriteItem[];
+
+  for (const { CollectionId, faces } of images) {
+    const output = faces.map(
+      ({ FaceId, ImageId, ExternalImageId, Confidence }) =>
+        ({
+          Put: {
+            TableName: dynamodb.tableName,
+            Item: marshall({
+              PK: `ALBUM#${CollectionId}`,
+              SK: `FACE#${FaceId}`,
+              FaceId,
+              ImageId,
+              Confidence,
+              CollectionId,
+              ExternalImageId,
+              CreatedAt: new Date().toISOString(),
+            }),
+          },
+        } as TransactWriteItem)
+    );
+
+    TransactItems.push(...output);
+  }
+
+  const batches = splitBatches(TransactItems, TRANSACTIONS_LIMIT_PER_BATCH);
+
+  for (const batch of batches) {
+    const command = new TransactWriteItemsCommand({
+      TransactItems: batch,
+    });
+
+    await dynamodb.send(command);
+  }
 };
